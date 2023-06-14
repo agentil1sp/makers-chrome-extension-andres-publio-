539a07bc9f97c9109a1f22386d9cb89debf90d6f
Andres Publio Gentile <a.gentile@correo.unimet.edu.ve>
2023-06-14 01:17:08 -0400

    feat: Add window focus functionality to popup window, this commit adds a function to set focus on a newly created popup window. The  function is used to create the popup window with the  property set to . Then, a callback function is used to update the focus of the newly created window using the  function.

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,11 @@
-console.log("This is a popup!")
+chrome.windows.create({
+  url: 'popup.html',
+  type: 'popup',
+  width: 400,
+  height: 400,
+  focused: true,
+  setSelfAsOpener: true
+}, function(window) {
+  chrome.windows.update(window.id, { focused: true });
+});
+
